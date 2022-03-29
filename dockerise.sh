04232e14b1738227def3d05913bfd9723f1c4b20
Fabio Pavesi <fabio@adamassoft.it>
2022-03-29 12:01:12 +0200

    Switched to x86

--- dockerise.sh
+++ dockerise.sh
@@ -11,6 +11,6 @@ npm version patch && \
 VERSION=$(cat package.json | jq -r ".version") && \
 cd EDI-T && \
 mvn package && \
-docker build -t $IMAGE_NAME:$VERSION -t $IMAGE_NAME:latest . && \
+docker build --platform linux/amd64 -t $IMAGE_NAME:$VERSION -t $IMAGE_NAME:latest . && \
 docker push $IMAGE_NAME:$VERSION && \
 docker push $IMAGE_NAME:latest
